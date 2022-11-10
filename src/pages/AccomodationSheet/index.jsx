e0cf385668ca36cfa53d4c6b24b165a45bbabad4
ice <ice@xicex.fr>
2022-11-10 10:46:31 +0100

    maj

--- src/pages/AccomodationSheet/index.jsx
+++ src/pages/AccomodationSheet/index.jsx
@@ -1,48 +1,46 @@
 import React from "react";
-import styles from "pages/AccomodationSheet/AccomodationSheet.module.scss";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import styles from "pages/AccomodationSheet/AccomodationSheet.module.scss";
 import Slide from "components/Slide";
 // import Menu from "components/Menu";
 import axios from "axios";
 
 const AccommodationSheet = () => {
-  const [acco, setAcco] = useState([]);
+  const [accommodation, setAccommodation] = useState([]);
   const { id } = useParams();
-  console.log(id);
 
   useEffect(() => {
     const fetchDatas = async () => {
       const response = await axios("/logements.json");
-      const acco = response.data.find((accoId) => accoId.id === id);
-      setAcco(acco);
-      console.log("updated");
+      const currentAccommodation = response.data.find(
+        (accommodation) => accommodation.id === id
+      );
+      setAccommodation(currentAccommodation);
     };
-    fetchDatas({});
-    console.log("mounted");
-  }, []);
+    fetchDatas();
+  }, [id]);
 
-  console.log(acco);
+  console.log(accommodation);
 
-  return (
-    <>
-      <Slide />
-      <div>
-        <h1 className={styles.title}>{acco.title}</h1>
-        <p className={styles.location}>{acco.location}</p>
-      </div>
-      <div className={styles.tags}>
-        {acco.tags.map((tag, index) => {
-          console.log(tag, index);
-          return (
-            <div>
-              {tag}, {index}
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+  if (accommodation != []) {
+    return (
+      <>
+        <Slide />
+        <h1 className={styles.title}>{accommodation.title}</h1>
+        <p className={styles.location}>{accommodation.location}</p>
+        <div className={styles.tags}>
+          {accommodation.tags.map((tag, index) => {
+            return (
+              <div>
+                {tag}, {index}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 };
 
 export default AccommodationSheet;
